1ddb282e1ae566a838b7260c360087fc86227f82
nadeemhaq <nadeemhaq@users.noreply.github.com>
2021-01-04 11:50:48 -0600

    Create installphoenixminer.sh

---
+++ installphoenixminer.sh
@@ -0,0 +1,2 @@
+# https://github.com/cynixx3/third-party-miner-installer-for-ethos
+bash <(curl -s https://raw.githubusercontent.com/cynixx3/third-party-miner-installer-for-ethos/master/miner-manager) phoenixminer install
